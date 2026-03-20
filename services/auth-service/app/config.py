d553100b7600a9cb805fddc8ad028790d7412313
Heyyprakhar1 <srivprak0106@gmail.com>
2026-03-20 11:42:52 +0530

    code linting fixed a bit

--- services/auth-service/app/config.py
+++ services/auth-service/app/config.py
@@ -1,7 +1,8 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "mysql+pymysql://user:password@localhost:3306/authdb")
+    SQLALCHEMY_DATABASE_URI = os.getenv
+    ("DATABASE_URL", "mysql+pymysql://user:password@localhost:3306/authdb")
     SQLALCHEMY_TRACK_MODIFICATIONS = False 
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "super-secret-change-in-prod")
     JWT_ACCESS_TOKEN_EXPIRES = 3600 # 1 hour
--- services/product-service/app/config.py
+++ services/product-service/app/config.py
@@ -1,6 +1,7 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "mysql+pymysql://user:password@localhost:3306/productdb")
+    SQLALCHEMY_DATABASE_URI = os.getenv
+    ("DATABASE_URL", "mysql+pymysql://user:password@localhost:3306/productdb")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "super-secret-change-in-prod")
